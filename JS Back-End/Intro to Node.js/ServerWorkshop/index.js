9cbdc0a4fa0694a483cc1f6bc9f9e0ed771cfb23
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-05-21 13:10:16 +0300

    Update index.js

--- JS Back-End/Intro to Node.js/ServerWorkshop/index.js
+++ JS Back-End/Intro to Node.js/ServerWorkshop/index.js
@@ -1,24 +1,11 @@
 const http = require("http");
 const router = require("./router.js");
+const homeController = require("./controllers/homeControler.js")
 
-router.registerHandler("/", (req, res) => {
-  res.write(homePage);
-  res.end();
-});
+router.registerHandler("/", homeController)
 
 const port = 3000;
 const server = http.createServer(requestHandler);
-
-const homePage = `
-<html>
-<body>
-  <div>
-    <h1>The Page</h1>
-    <p>Welcome to the page!</p>
-  </div>
-</body>
-</html>
-`;
 
 function requestHandler(req, res) {
   console.log(">>>", req.method, req.url);
@@ -26,4 +13,4 @@ function requestHandler(req, res) {
   handler(req, res)
 }
 
-server.listen(3000);
+server.listen(port, () => console.log("Server listening on port" + port));
