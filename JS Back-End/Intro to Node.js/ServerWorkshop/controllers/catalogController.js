4ddceb95fd7b397526eab71cb689308d04b03d31
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-05-22 11:26:03 +0300

    Update catalogController.js

--- JS Back-End/Intro to Node.js/ServerWorkshop/controllers/catalogController.js
+++ JS Back-End/Intro to Node.js/ServerWorkshop/controllers/catalogController.js
@@ -1,18 +1,25 @@
 const layout = require("../views/layout.js")
+const database = require("../util/database.js")
 
-const html = `
+const html = (items) => `
 <div>
   <h1>Catalog</h1>
+  <form method="POST" action="/create">
+  <label>Name <input type="text" name="name">
+  <label>S/N <input type="text" name="serial">
+  <input type="submit" value="Create Item">
+  </from>
   <ul>
-  <li>First Item</li>
-  <li>Second Item</li>
-  <li>Third Item</li>
+  ${items.map(item => `<li>${item.name} - ${item.serial}</li>`)}
+    
+    <li>Second Item</li>
+    <li>Third Item</li>
   </ul>
 </div>
 `;
 
 module.exports = (req, res) => {
-  res.write(layout(html))
+  res.write(layout(html(database)))
   res.end()
 }
 
