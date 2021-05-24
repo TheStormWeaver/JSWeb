34fc7eb79a3be41952a966436773acccd6ed4f58
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-05-24 12:21:12 +0300

    Create index.js

---
+++ JS Back-End/NodeJs Sterms and Utilities/ServerWorkshop(Updated)/index.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const url = require("url")
+
+const router = require("./router.js");
+
+const homeController = require("./controllers/homeControler.js");
+const aboutController = require("./controllers/aboutController.js");
+const catalogController = require("./controllers/catalogController.js");
+const createController = require("./controllers/createController.js");
+const deleteController = require("./controllers/deleteController.js");
+
+router.get("/", homeController)
+router.get("/about", aboutController)
+router.get("/catalog", catalogController)
+
+router.post("/create", createController)
+router.get("/delete", deleteController)
+
+const port = 3000;
+const server = http.createServer(requestHandler);
+
+function requestHandler(req, res) {
+  const url = new URL(req.url, "http://localhost")
+  console.log(">>>", req.method, req.url, req.path, req.pathname);
+  const handler = router.match(req.method, url.pathname)
+  handler(req, res)
+}
+
+server.listen(port, () => console.log("Server listening on port" + port));
