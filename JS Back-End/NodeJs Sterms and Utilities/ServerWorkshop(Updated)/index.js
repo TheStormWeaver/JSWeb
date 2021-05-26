04c8c2164dad85522de1a64370eda30f81a912e4
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-05-26 18:50:21 +0300

    Update index.js

--- JS Back-End/NodeJs Sterms and Utilities/ServerWorkshop(Updated)/index.js
+++ JS Back-End/NodeJs Sterms and Utilities/ServerWorkshop(Updated)/index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const url = require("url")
+const url = require("url");
 
 const router = require("./router.js");
 
@@ -8,22 +8,25 @@ const aboutController = require("./controllers/aboutController.js");
 const catalogController = require("./controllers/catalogController.js");
 const createController = require("./controllers/createController.js");
 const deleteController = require("./controllers/deleteController.js");
+const uploadController = require("./controllers/uploadController.js");
 
-router.get("/", homeController)
-router.get("/about", aboutController)
-router.get("/catalog", catalogController)
+router.get("/", homeController);
+router.get("/about", aboutController);
+router.get("/catalog", catalogController);
 
-router.post("/create", createController)
-router.get("/delete", deleteController)
+router.post("/create", createController);
+router.get("/delete", deleteController);
+router.post("/upload", uploadController);
 
 const port = 3000;
 const server = http.createServer(requestHandler);
 
 function requestHandler(req, res) {
-  const url = new URL(req.url, "http://localhost")
+  const url = new URL(req.url, "http://localhost");
   console.log(">>>", req.method, req.url, req.path, req.pathname);
-  const handler = router.match(req.method, url.pathname)
-  handler(req, res)
+  const handler = router.match(req.method, url.pathname);
+  handler(req, res);
 }
 
 server.listen(port, () => console.log("Server listening on port" + port));
+
