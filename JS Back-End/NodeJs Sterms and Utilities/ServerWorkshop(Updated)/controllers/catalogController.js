897ed5c11d079877e8ba8b3a17369cf77c80ad33
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-05-26 18:54:15 +0300

    Update catalogController.js

--- JS Back-End/NodeJs Sterms and Utilities/ServerWorkshop(Updated)/controllers/catalogController.js
+++ JS Back-End/NodeJs Sterms and Utilities/ServerWorkshop(Updated)/controllers/catalogController.js
@@ -1,25 +1,15 @@
-const { layout } = require("../util/template.js");
+const { layout, render } = require("../util/template.js");
 const database = require("../util/database.js");
 
-const html = (items) => `
-<div>
-  <h1>Catalog</h1>
-  <form method="POST" action="/create">
-  <label>Name <input type="text" name="name">
-  <label>S/N <input type="text" name="serial">
-  <input type="submit" value="Create Item">
-  </from>
-  <ul>
-  ${items.map(([id, item]) => `<li data-id="${id}">${item.name} - ${item.serial} <a href="/delete?id=${id}">[Delete]</a> </li>`).join("")}
-    
-    <li>Second Item</li>
-    <li>Third Item</li>
-  </ul>
-</div>
-`;
-
 module.exports = async (req, res) => {
-  res.write(await layout(html(Object.entries(database.database))))
-  res.end()
-}
-
+  const catalogPage = await render("catalog", {
+    items: Object.entries(database.database)
+      .map(
+        ([id, item]) =>
+          `<li data-id="${id}">${item.name} - ${item.serial} <a href="/delete?id=${id}">[Delete]</a> </li>`
+      )
+      .join(""),
+  });
+  res.write(await layout(catalogPage, "Catalog"));
+  res.end();
+};
