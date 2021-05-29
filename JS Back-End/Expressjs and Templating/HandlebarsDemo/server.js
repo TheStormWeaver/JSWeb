edc5aa0bf59d62385cfbf9367b0b57a2fe86005f
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-05-29 23:10:23 +0300

    Update server.js

--- JS Back-End/Expressjs and Templating/HandlebarsDemo/server.js
+++ JS Back-End/Expressjs and Templating/HandlebarsDemo/server.js
@@ -1,40 +1,45 @@
-const express = require("express")
-const hbs = require("express-handlebars")
+const express = require("express");
+const hbs = require("express-handlebars");
 
-const app = express()
+const app = express();
 
-app.engine("handlebars", hbs({
-  extname: ".hbs"
-}))
+app.engine(
+  "handlebars",
+  hbs({
+    extname: ".hbs",
+  })
+);
 
-app.set("view engine", ".hbs")
+app.set("view engine", ".hbs");
 
 app.get("/", (req, res) => {
   const data = {
     user: {
-      username: "Peter"
+      username: "Peter",
     },
     title: "Home Page",
     name: "Peter",
     age: 24,
-    items: [{
-      type: "Lint",
-      qty: 5
-    }, 
-    {
-      type: "Wallet",
-      qty: 1
-    },
-    {
-      type: "Bubblegum",
-      qty: 10
-    },
-    {
-      type: "Coins",
-      qty: 3.5
-    }]
-  }
-  res.render("home.hbs", data)
-})
+    items: [
+      {
+        type: "Lint",
+        qty: 5,
+      },
+      {
+        type: "Wallet",
+        qty: 1,
+      },
+      {
+        type: "Bubblegum",
+        qty: 10,
+      },
+      {
+        type: "Coins",
+        qty: 3.5,
+      },
+    ],
+  };
+  res.render("home.hbs", data);
+});
 
-app.listen(3030)
+app.listen(3030);
