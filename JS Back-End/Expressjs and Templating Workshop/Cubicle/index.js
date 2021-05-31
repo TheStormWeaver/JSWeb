5180f93fe20927a4052e33ecdfd82e93f06acc12
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-05-31 18:55:57 +0300

    Add files via upload

--- JS Back-End/Expressjs and Templating Workshop/Cubicle/index.js
+++ JS Back-End/Expressjs and Templating Workshop/Cubicle/index.js
@@ -1,28 +1,35 @@
 const express = require("express")
 const hbs = require("express-handlebars")
 
+const { init: storage } = require("./models/storage")
+
 const { about } = require("./controllers/about")
 const { catalog } = require("./controllers/catalog")
 const { create, post } = require("./controllers/create")
 const { details } = require("./controllers/details")
 const { notFound } = require("./controllers/notFound")
 
-const port = 3030
-const app = express()
+start()
 
-app.engine("hbs", hbs({
-  extname: ".hbs"
-}))
-app.set("view engine", "hbs")
-app.use("/static", express.static("static"))
+async function start() {
+  const port = 3030
+  const app = express()
+  
+  app.engine("hbs", hbs({
+    extname: ".hbs"
+  }))
+  app.set("view engine", "hbs")
+  app.use("/static", express.static("static"))
+  app.use(express.urlencoded({extended: false}))
+  app.use(await storage())
+  
+  app.get("/", catalog)
+  app.get("/about", about)
+  app.get("/details/:id", details)
+  app.get("/create", create)
+  app.post("/create", post)
+  
+  app.all("*", notFound)
 
-app.get("/", catalog)
-app.get("/about", about)
-app.get("/details/:id", details)
-app.get("/create", create)
-app.post("/create", post)
-
-app.all("*", notFound)
-
-
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+  app.listen(port, () => console.log(`Server listening on port ${port}`))
+}
