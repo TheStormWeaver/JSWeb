6cc6f8b21210a64960bb48012acc312ff27aec95
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-03 18:12:28 +0300

    Update index.js

--- JS Back-End/NoSQL and MongoDB/Mongoose Demo/index.js
+++ JS Back-End/NoSQL and MongoDB/Mongoose Demo/index.js
@@ -12,8 +12,23 @@ async function start() {
   console.log("Database connected");
 
   const catSchema = new mongoose.Schema({
-    name: String,
-    color: String
+    name: {
+      type: String,
+      required: true,
+      validator: function(value) {
+        const letter = value.slice(0, 1)
+        return latter === letter.toUpperCase()
+      },
+      message: "Invalid name"
+    },
+    color: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["Grey", "Orange", "Turquoise", "White"],
+        message: "Color must be one of the listed"
+      }
+    }
   })
   const Cat = mongoose.model("Cat", catSchema)
 
@@ -23,8 +38,5 @@ async function start() {
   })
   
   await myCat.save()
-
-  const data = await Cat.find({})
-
-  console.log(data)
 }
+
