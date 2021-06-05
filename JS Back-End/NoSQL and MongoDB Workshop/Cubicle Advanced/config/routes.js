ea1c87b977d6968bbd739cfb9ba4e15a4321df71
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-05 16:06:02 +0300

    Add files via upload

---
+++ JS Back-End/NoSQL and MongoDB Workshop/Cubicle Advanced/config/routes.js
@@ -0,0 +1,19 @@
+const { about } = require("../controllers/about")
+const { catalog } = require("../controllers/catalog")
+const { create, createPost } = require("../controllers/create")
+const { details } = require("../controllers/details")
+const { edit, editPost } = require("../controllers/edit")
+const { notFound } = require("../controllers/notFound")
+
+module.exports = (app) => {
+  app.get("/", catalog);
+  app.get("/about", about);
+  app.get("/details/:id", details);
+  app.get("/create", create);
+  app.post("/create", createPost);
+
+  app.get("/edit/:id", edit);
+  app.post("/edit/:id", editPost);
+
+  app.all("*", notFound);
+};
