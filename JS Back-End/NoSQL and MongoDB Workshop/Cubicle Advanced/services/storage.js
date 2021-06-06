61f6559af07e067ba5dd941fd0bcda7781aad8e5
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-06 16:31:11 +0300

    Add files via upload

--- JS Back-End/NoSQL and MongoDB Workshop/Cubicle Advanced/services/storage.js
+++ JS Back-End/NoSQL and MongoDB Workshop/Cubicle Advanced/services/storage.js
@@ -1,16 +1,6 @@
-const Cube = require("../models/Cube")
-const fs = require("fs/promises");
-const uniqid = require("uniqid");
-
-let data = {};
+const Cube = require("../models/Cube");
 
 async function init() {
-  try {
-    data = JSON.parse(await fs.readFile("./models/data.json"));
-  } catch (err) {
-    console.error("Error reading database");
-  }
-
   return (req, res, next) => {
     req.storage = {
       getAll,
@@ -23,52 +13,45 @@ async function init() {
 }
 
 async function getAll(query) {
-  const cubes = Cube.find({}).lean()
-  /*
+  const options = {};
+
   if (query.search) {
-    cubes = cubes.filter((c) =>
-      c.name.toLowerCase().includes(query.search.toLowerCase())
-    );
+    options.name = { $regex: query.search, $options: "i" };
   }
   if (query.from) {
-    cubes = cubes.filter((c) => c.difficulty >= Number(query.from));
+    options.difficulty = { $gte: Number(query.from) };
   }
   if (query.to) {
-    cubes = cubes.filter((c) => c.difficulty <= Number(query.to));
+    options.difficulty = options.difficulty || {}
+    options.difficulty.$lte = Number(query.to) ;
   }
-  */
-  return cubes; 
+
+  const cubes = Cube.find(options).lean();
+
+  return cubes;
 }
 
 async function getById(id) {
-  const cube = await Cube.findById(id).lean()
+  const cube = await Cube.findById(id).lean();
   if (cube) {
-    return cube
+    return cube;
   } else {
     return undefined;
   }
 }
 
 async function create(cube) {
-  const record = new Cube(cube)
-  return record.save()
+  const record = new Cube(cube);
+  return record.save();
 }
 
 async function edit(id, cube) {
-  const existing = await Cube.findById(id)
+  const existing = await Cube.findById(id);
   if (!existing) {
     throw new ReferenceError("No such ID in database");
   }
-  Object.assign(existing, cube)
-  return existing.save()
-}
-
-async function persist() {
-  try {
-    await fs.writeFile("./models/data.json", JSON.stringify(data, null, 2));
-  } catch (err) {
-    console.error("Error writing out database");
-  }
+  Object.assign(existing, cube);
+  return existing.save();
 }
 
 module.exports = {
