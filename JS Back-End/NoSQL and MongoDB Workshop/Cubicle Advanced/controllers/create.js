2f3ba9179f581c786147309636467e22fd31cc8c
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-06 16:31:36 +0300

    Add files via upload

--- JS Back-End/NoSQL and MongoDB Workshop/Cubicle Advanced/controllers/create.js
+++ JS Back-End/NoSQL and MongoDB Workshop/Cubicle Advanced/controllers/create.js
@@ -1,15 +1,21 @@
 module.exports = {
   create: (req, res) => {
-    res.render("create", {title: "Create Cube"})
+    res.render("create", { title: "Create Cube" });
   },
   createPost: async (req, res) => {
     const cube = {
       name: req.body.name,
       description: req.body.description,
       imageUrl: req.body.imageUrl,
-      difficulty: Number(req.body.difficulty)
+      difficulty: Number(req.body.difficulty),
+    };
+    try {
+      await req.storage.create(cube);
+    } catch (err) {
+      if(err.name == "ValidationError"){
+        return res.render("create", {title: "Create Cube", error: "All fields are required. Image URL must a valid url"})
+      }
     }
-    await req.storage.create(cube)
-    res.redirect("/")
-  }
-}
+    res.redirect("/");
+  },
+};
