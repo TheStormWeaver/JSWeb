bb0cefab81738641e25fc097f1c611876a5bbe56
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-07 21:42:09 +0300

    Add files via upload

--- JS Back-End/NoSQL and MongoDB Workshop/Cubicle Advanced/controllers/details.js
+++ JS Back-End/NoSQL and MongoDB Workshop/Cubicle Advanced/controllers/details.js
@@ -1,14 +1,34 @@
 module.exports = {
   details: async (req, res) => {
-    const cube = await req.storage.getById(req.params.id)
-    if(cube === undefined){
-      res.redirect("/404")
-    }else {
+    const cube = await req.storage.getById(req.params.id);
+    if (cube === undefined) {
+      res.redirect("/404");
+    } else {
       const ctx = {
         title: "Cubicle",
-        cube
-      }
-      res.render("details", ctx)
+        cube,
+      };
+      res.render("details", ctx);
     }
-  }
-}
+  },
+  async attach(req, res) {
+    const cube = await req.storage.getById(req.params.id);
+    const accessories = await req.storage.getAllAccessories(
+      (cube.accessories || []).map((a) => a._id)
+    );
+
+    res.render("attach", {
+      title: "Attach stickers",
+      cube,
+      accessories,
+    });
+  },
+  async attachPost(req, res) {
+    const cubeId = req.params.cubeId;
+    const stickerId = req.body.accessory;
+
+    await req.storage.attachSticker(cubeId, stickerId);
+
+    res.redirect(`/details/${cubeId}`);
+  },
+};
