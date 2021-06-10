1c8953d62a1157061ea210e578ffa06738dcdae7
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-10 13:22:56 +0300

    Update server.js

--- JS Back-End/Session and Authentication/Demo/CookieDemo/server.js
+++ JS Back-End/Session and Authentication/Demo/CookieDemo/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -6,18 +7,12 @@ const sessions = {};
 
 function mySessionStorage(req, res, next) {
   let session = {};
-  if (req.header.cookie) {
-    const id = req.headers.cookie.split("=")[1];
-    if (sessions[id] == undefined) {
-      console.log(">>>> Invalid session cookie, generating new");
-      createSession()
-    } else {
-      session = sessions[id];
-      console.log(">>>> Existing session", session);
-    }
+  if (req.cookies.sessionId && sessions[req.cookies.sessionId] != undefined) {
+    const id = req.cookies.sessionId;
+    session = sessions[id];
     console.log(">>>> Existing session", session);
   } else {
-    createSession()
+    createSession();
   }
 
   req.session = session;
@@ -25,25 +20,23 @@ function mySessionStorage(req, res, next) {
   next();
 
   function createSession() {
-    const id =
-      "00000000" + ((Math.random() * 99999999) | 0).toString(16).slice(-8);
+    const id = "00000000" + ((Math.random() * 99999999) | 0).toString(16).slice(-8);
     sessions[id] = session;
-
-    res.setHeader("Set-Cookie", `sessionId=${id}`);
+    
+    res.cookie("sessionId", id)
     console.log("New user, generated session with ID", id);
 
     session.visited = 0;
   }
 }
 
+app.use(cookieParser());
 app.use(mySessionStorage);
 
 app.get("/", (req, res) => {
   req.session.visited++;
-
-  res.send(
-    `<h1>Hello</h1><p>Your session has data ${JSON.stringify(req.session)}.</p>`
-  );
+  res.send(`<h1>Hello</h1><p>Your session has data ${JSON.stringify(req.session)}.</p>`);
 });
 
 app.listen(3030);
+
