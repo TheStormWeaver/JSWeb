e026d768c1506f6926f44d93817c6946f2b1b5d3
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-11 12:32:43 +0300

    Create controllers.js

---
+++ JS Back-End/Session and Authentication/Demo/AuthenticationDemo/controllers.js
@@ -0,0 +1,56 @@
+module.exports = (app) => {
+  app.all("*", (req, res, next) => {
+    if (req.url.includes("favicon") == false) {
+      console.log(">>>", req.method, req.url, req.body);
+      console.log(">>> Session data", req.session);
+    }
+    next();
+  });
+
+  app.get("/", (req, res) => {
+    let title = "Welcome"
+    if(req.session.user){
+      title = "Welcome, " + req.session.user.username
+    }
+    res.send(layout("<p>Home Page</p>", title));
+  });
+
+  app.get("/register", (req, res) => {
+    let title = "Welcome"
+    if(req.session.user){
+      title = "Welcome, " + req.session.user.username
+    }
+    res.send(layout(`
+    <form action="/register" method="POST">
+    <label>Username: <input type="text" name="username"></label>
+    <label>Password: <input type="password" name="password"></label>
+    <label>Repeat Pass: <input type="password" name="repass"></label>
+    <input type="submit" name="Register">
+    </form> 
+    `, title));
+  });
+
+  app.get("/login", (req, res) => {
+    let title = "Welcome"
+    if(req.session.user){
+      title = "Welcome, " + req.session.user.username
+    }
+    res.send(layout(`
+    <form action="/login" method="POST">
+    <label>Username: <input type="text" name="username"></label>
+    <label>Password: <input type="password" name="password"></label>
+    <input type="submit" name="Login">
+    </form> 
+    `, title));
+  });
+};
+
+function layout(html, title) {
+  return `
+  <h1>${title}</h1>
+  <a href="/">Home</a>
+  <a href="/register">Register</a>
+  <a href="/login">Login</a>
+  ${html}
+  `;
+}
