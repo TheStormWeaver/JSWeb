ab6281cb52cf8364d21db009db73b002b6f484f6
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-11 12:48:58 +0300

    Create auth.js

---
+++ JS Back-End/Session and Authentication/Demo/AuthenticationDemo/auth.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcrypt");
+
+const users = {};
+
+module.exports = (req, res, next) => {
+  req.register = async (username, password) => {
+    const id = (("00000000" + Math.random() * 99999999) | 0)
+      .toString(16)
+      .slice(-4);
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    users[id] = {
+      username: req.body.username,
+      hashedPassword,
+    };
+    console.log("New user", users);
+  };
+
+  req.login = async (username, password) => {
+    const user = Object.entries(users).find(
+      ([id, u]) => u.username == username
+    );
+    console.log(user);
+
+    console.log("Checking password", password, "for user", user);
+
+    if (user && await bcrypt.compare(req.body.password, user[1].hashedPassword)) {
+      req.session.user = {
+        _id: user[1].id,
+        username,
+      };
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  next();
+};
