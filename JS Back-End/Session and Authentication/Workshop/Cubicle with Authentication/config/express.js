c1431d03cae1bdefc642ee4e2d1d305cc4dfd693
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-13 12:19:29 +0300

    Add files via upload

--- JS Back-End/Session and Authentication/Workshop/Cubicle with Authentication/config/express.js
+++ JS Back-End/Session and Authentication/Workshop/Cubicle with Authentication/config/express.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const hbs = require("express-handlebars")
+const cookieParser = require("cookie-parser")
+
+const auth = require("../middlewares/auth")
 
 module.exports = (app) => {
   app.engine(
@@ -12,4 +15,6 @@ module.exports = (app) => {
   app.use("/static", express.static("static"));
   app.use("/js", express.static("js"));
   app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser())
+  app.use(auth())
 };
