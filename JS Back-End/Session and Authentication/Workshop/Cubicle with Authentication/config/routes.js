fe5291027142f8f8835c6f36ffff28bf8fca1998
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-14 12:29:15 +0300

    Add files via upload

--- JS Back-End/Session and Authentication/Workshop/Cubicle with Authentication/config/routes.js
+++ JS Back-End/Session and Authentication/Workshop/Cubicle with Authentication/config/routes.js
@@ -4,13 +4,14 @@ const productController = require("../controllers/productController")
 const accessoryController = require("../controllers/accessoryController")
 const homeController = require("../controllers/homeController")
 const authController = require("../controllers/authController")
+const { isAuth } = require("../middlewares/guards")
 
 module.exports = (app) => {
   app.use("/products", productController)
   app.use("/accessory", accessoryController)
   app.use("/auth", authController)
 
-  app.post("/comments/:cubeId/create", commentsPost)
+  app.post("/comments/:cubeId/create", isAuth(), commentsPost)
 
   app.use("/", homeController)
 };
