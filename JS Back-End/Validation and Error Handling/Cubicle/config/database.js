3da4f5945be13edf2dbe3d56d93dc301d87b716f
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-16 14:17:43 +0300

    Add files via upload

--- JS Back-End/Validation and Error Handling/Cubicle/config/database.js
+++ JS Back-End/Validation and Error Handling/Cubicle/config/database.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
     mongoose.connect("mongodb://localhost:27017/cubicle", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
+      autoIndex: false,
     });
   
     const db = mongoose.connection
