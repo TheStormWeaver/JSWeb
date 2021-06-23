b477f92fd306a9c3257de38487a229d28027409b
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-06-23 17:35:16 +0300

    Add files via upload

---
+++ JS Back-End/Exam Prep/Theater/models/Play.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+const schema = new Schema({
+  title: { type: String, required: [true, "A name for the play is required!"] },
+  description: {
+    type: String,
+    required: [true, "Description for the play is required!"],
+    maxLength: [50, "Description must be less than 50 characters long!"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "An image with a valid URL is required!"],
+    match: [/^https?/, "Image must be a valid URL"],
+  },
+  public: { type: Boolean, required: true, default: false },
+  createdAt: { type: Date, default: Date.now },
+  usersLiked: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+module.exports = model("Play", schema);
