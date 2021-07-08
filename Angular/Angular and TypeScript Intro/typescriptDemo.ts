42b6ea6cc1965673d3d3ed74291ab218ba491f23
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-07-08 13:02:13 +0300

    Update typescriptDemo.ts

--- Angular/Angular and TypeScript Intro/typescriptDemo.ts
+++ Angular/Angular and TypeScript Intro/typescriptDemo.ts
@@ -1,10 +1,41 @@
+enum UserRole {
+  Admin,
+  Client,
+}
+
+interface IUserNewData {
+  newName: string;
+  newAge: number;
+}
+
 class Person {
-  name;
-  age;
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+  constructor(
+    public name: string,
+    public age: number,
+    public role: UserRole
+  ) {}
+
+  setData(newData: IUserNewData) {
+    this.name = newData.newName;
+    this.age = newData.newAge;
+  }
+
+  getAge(): number {
+    return this.age;
   }
 }
 
-const ivan = new Person("Ivan", 30)
+const ivan = new Person("Ivan", 30, UserRole.Admin);
+
+const newData: IUserNewData = { newName: "Peter", newAge: 20 };
+ivan.setData(newData);
+
+function idnentity<T>(arg: T): T {
+  return arg
+}
+
+const test = UserRole.Client
+
+if (test == ivan.role) {
+  console.log(ivan)
+}
