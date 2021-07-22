688c8d36d08bdff4cd6ca49e47532637707a52cf
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-07-22 17:36:45 +0300

    Add files via upload

---
+++ Angular/Modules and Routing/first-app/src/main.ts
@@ -0,0 +1,36 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+// event emitter explanation and example
+/* 
+class MyEventEmitter<T> {
+  subscriptions: ((data: T) => void)[] = [];
+
+  subscribe(fn: (data: T) => void): void {
+    this.subscriptions.push(fn);
+  }
+
+  emit(data: T): void {
+    this.subscriptions.forEach((fn) => fn(data));
+  }
+}
+
+const emitter = new MyEventEmitter();
+
+emitter.subscribe(console.log);
+
+setTimeout(() => {
+  emitter.emit(100);
+}, 1000);
+*/
