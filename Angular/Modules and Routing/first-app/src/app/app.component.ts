0bae90339a5b5177543dbd23341fbcbb8ae79e88
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-07-22 17:39:52 +0300

    Add files via upload

---
+++ Angular/Modules and Routing/first-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  actiavteHandler(data: any): void {
+    console.log(data)
+  }
+}
---
+++ Angular/Modules and Routing/first-app/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { InjectionToken, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const myStringInjectionToken = new InjectionToken("myString")
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    NotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CoreModule,
+    SharedModule,
+    UserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    {
+      provide: myStringInjectionToken,
+      useValue: "Hello World!"
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
