563681f9795b4a8e599249f33d3ff7e1434cf55c
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-07-30 15:15:19 +0300

    Update app-routing.module.ts

--- Angular/ProjectPrep/idea-app/src/app/app-routing.module.ts
+++ Angular/ProjectPrep/idea-app/src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: 'full'},
-  { path: "login", component: LoginComponent},
-  { path: "register", component: RegisterComponent},
-  { path: "profile", component: ProfileComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
